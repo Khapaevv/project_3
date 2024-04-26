476561af842e024a0e68955d78294e0eaf02d9e5
Vladimir Khapaev <khapaev@mail.ru>
2024-04-26 08:46:27 +0300

    some differents

--- trash/other.py
+++ trash/other.py
@@ -2,12 +2,26 @@ import json
 
 
 def load_operations():
-    with open('../operations.json', ) as f:
+    with open('operations.json', ) as f:
         return json.load(f)
 
-print(load_operations())
+# print(load_operations())
+list_executed = []
+def executed_operations():
+    for i in load_operations():
+        print(i)
+        if i["state"] == "EXECUTED":
+            list_executed.append(i)
+            print(list_executed)
+        # load_operations()
+
+            # list_executed.append(i)
+            # return list_executed
+#
+print(executed_operations())
 
 
+# print(load_operations()[state])
 
 # def get_student_by_pk(pk):
 #     ls = load_students()
@@ -18,5 +32,3 @@ print(load_operations())
 
 
 
-# file = open("example.json")
-# json.load(file)
